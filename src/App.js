ef240546d4532d6655501dd8650aba492c920e20
Sourabh Kumar <95950369+sour7@users.noreply.github.com>
2022-08-10 15:23:26 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Forms } from './components/Forms';
 import './App.css';
-// import { useRef } from 'react';
+
 import { useState } from 'react';
 
 function App() {
@@ -18,18 +18,19 @@ function App() {
       name: 'username',
       type: 'text',
       placeholder: 'Username',
-      errorMessage:"username should be 3-16 character and shouldn't include any special character ",
-      required:true,
+      errorMessage:
+        "username should be 3-16 character and shouldn't include any special character ",
+      required: true,
       label: 'Username',
-      pattern:"^[A-Za-z0-9]{3,16}$"
+      pattern: '^[A-Za-z0-9]{3,16}$',
     },
     {
       id: 2,
       name: 'email',
       type: 'email',
       placeholder: 'Email',
-      errorMessage:"email address is not valid",
-      required:true,
+      errorMessage: 'email address is not valid',
+      required: true,
       label: 'Email',
     },
     {
@@ -44,34 +45,27 @@ function App() {
       name: 'password',
       type: 'password',
       placeholder: 'Password',
-      errorMessage:"greater than 8 character and must include 1 number, 1 letter, 1 special character ",
-      required:true,
+      errorMessage:
+        'greater than 8 character and must include 1 number, 1 letter, 1 special character ',
+      required: true,
       label: 'Password',
-      pattern:`^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,20}$`,
+      pattern: `^(?=.*[0-9])(?=.*[A-Za-z])(?=.*[@$!%*#?&])[A-Za-z0-9@$!%*#?&]{8,20}$`,
     },
     {
       id: 5,
       name: 'confirmPassword',
       type: 'password',
       placeholder: 'ConfirmPassword',
-      errorMessage:"password doesn't match",
-      required:true,
+      errorMessage: "password doesn't match",
+      required: true,
       label: 'Confirm Password',
-      pattern:values.password,
+      pattern: values.password,
     },
   ];
 
-  // console.log(username);
-  // console.log('re-rendered');
-
-  // const usernameRef = useRef()
-  // console.log(usernameRef)
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(usernameRef)
-    // const data= new FormData(e.target)
-    // console.log(Object.fromEntries(data.entries()))
+    //post request
   };
 
   const onChange = (e) => {
@@ -83,9 +77,13 @@ function App() {
     <div className="App">
       <h1>Registration</h1>
       <form onSubmit={handleSubmit}>
-        {/* <Forms placeholder="username" refer={usernameRef} /> */}
         {inputs.map((input) => (
-          <Forms key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+          <Forms
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
         ))}
         <button>Submit</button>
       </form>
